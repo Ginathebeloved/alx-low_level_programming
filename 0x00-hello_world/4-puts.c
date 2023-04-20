00c01b22dc6d3df95515d3a0416eed2504c8e08d
Gina <gina.thebeloved@gmail.com>
2023-04-20 07:22:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * Intializing the string
+ * main -entry point
  *
- * return sucess
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 }
