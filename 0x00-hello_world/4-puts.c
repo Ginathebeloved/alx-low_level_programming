8b4780c173aee09b7193cb0758b7664a82b2aa54
Gina <gina.thebeloved@gmail.com>
2023-04-20 06:32:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+
+	/* initializingthe sting */
+	char string[] = "Programming is like building a multilingual puzzle\n";
+	
+	/* writting our string to stdout */
+	
+	puts(string);
+	return 0;
+}
