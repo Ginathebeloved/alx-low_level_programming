c9070cee095c40e437e06567c3a14b6cc13661fd
Gina <gina.thebeloved@gmail.com>
2023-04-21 20:57:57 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *Description: 'get the last difit of a number'
+ * Return: Always 0
+ */
+
+int main(void)
+{
