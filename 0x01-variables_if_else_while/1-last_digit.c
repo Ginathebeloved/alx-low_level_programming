88a16c014f085ba2702f7983b03a254b3b29f417
Gina <gina.thebeloved@gmail.com>
2023-04-21 19:42:00 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,28 +4,28 @@
 
 /**
  * main - Determines if a number is positive, negative or zero.
- *
- * Return: Always 0 (sucess)
+ *Description: 'get the last difit of a number'
+ * Return: Always 0
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-       
+
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
+		else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else
+		else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	return (0);
-
+		return (0);
 }
