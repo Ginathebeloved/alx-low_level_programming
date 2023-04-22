9d233259ba168cbeabcbb7a0f87b8bf57e168a63
Gina <gina.thebeloved@gmail.com>
2023-04-21 19:51:04 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *Description: 'get the last difit of a number'
+ * Return: Always 0
+ */
+
+int main(void)
+{
+		putchar(65);
+		putchar('\n');
+		return (0);
+}
