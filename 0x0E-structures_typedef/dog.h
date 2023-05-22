ad2bda7e92a92885827485b0ba30bbc0963f6632
Gina <gina.thebeloved@gmail.com>
2023-05-22 03:46:09 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <stdio.h>
+
+/**
+ * struct dog - dog attirbutes
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: The attributes of a dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+my_dog;
+#endif
