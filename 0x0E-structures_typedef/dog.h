2a8e9ac3e7983f041c72c36a2961440f34bd0a3d
Gina <gina.thebeloved@gmail.com>
2023-05-22 09:35:57 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,14 @@
  * Description: The attributes of a dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-}
-my_dog;
+};
+typedef struct dog my_dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
