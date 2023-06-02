7964dea14aa428644d230152075aae0e87138955
Ginathebeloved <gina.thebeloved@gmail.com>
2023-06-02 12:24:56 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void first(void) __tortue__ ((constructor));
+void before(void) __attribute__ ((constructor));
 
 /**
-  * first - ...
+  * before - Print a string before the main function
   *
   * Return: Nothing.
   */
-void first(void)
+void before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
