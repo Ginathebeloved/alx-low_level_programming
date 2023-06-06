86081aead0ac0e1855076bdf81fb59bf404a1136
Ginathebeloved <gina.thebeloved@gmail.com>
2023-06-06 10:45:12 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,8 +11,10 @@
 */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
 
-	for (unsigned int i = 0; i < index && head != NULL; i++)
+	for (i = 0; i < index && head != NULL; i++)
 		head = head->next;
 	return (head);
 }
