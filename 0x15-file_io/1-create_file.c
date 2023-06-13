03d805f690c8e5d9db92a2b5b264cff10410287b
Ginathebeloved <gina.thebeloved@gmail.com>
2023-06-13 10:37:59 -0700

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length]; lenght++)
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
